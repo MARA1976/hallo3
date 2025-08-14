b11c04b6b9a07158306ee820f025b059100f568d
ahlem <oueslatiahlem76@gmail.com>
2025-08-14 18:57:13 +0200

    chore: remove tracked files and update gitignore rules

--- hallo3_app.py
+++
@@ -1,8 +0,0 @@
-# hallo3_app.py
-from app import create_gradio_interface
-
-if __name__ == "__main__":
-    interface = create_gradio_interface()
-    interface.launch(server_name="0.0.0.0", server_port=7860)
-
-
